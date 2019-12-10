d4cf62d3d4388d2d3919d482b8d5ceb11635ce87
aschnee <alina.schneeberger@htl-salzburg.ac.at>
2019-12-10 17:27:41 +0100

    deleted not needed methode SubNumbers

--- Assets/MyGame/Scripts/CommputeNumbers.cs
+++ Assets/MyGame/Scripts/CommputeNumbers.cs
@@ -15,31 +15,23 @@ public class CommputeNumbers : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SubNumbers(varA, varB);
-        SubNumbers(4, 7);
         InitVariables();
     }
+
     public void InitVariables()
     {
         varA = 0;
         varB = 0;
     }
+
     public void SetResult()
     {
         float a = float.Parse(ipVarA.text);
         float b = float.Parse(ipVarB.text);
-        Debug.Log("in SetResult" + (a - b));
         //result.text = SubstractNumbers(a, b);
         float resultAB = a - b;
         result.text = resultAB.ToString();
 
-    }
-
-    public float SubNumbers(float variableA, float variableB)
-    {
-        float result = variableA - variableB;
-        //Debug.Log("Test sub" + result.ToString());
-        return result;
     }
     // Update is called once per frame
     void Update()
